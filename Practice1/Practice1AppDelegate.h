dfc009b6cb2f10a10cf977280a76d7028b7f9a79
Michael Toth <michael@virtualpianist.com>
2014-02-13 12:32:30 -0500

    Initial Commit

---
+++ Practice1/Practice1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Practice1AppDelegate.h
+//  Practice1
+//
+//  Created by Michael Toth on 2/13/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Practice1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Practice1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice1
+//
+//  Created by Michael Toth on 2/13/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Practice1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Practice1AppDelegate class]));
+    }
+}
